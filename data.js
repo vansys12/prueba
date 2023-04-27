23f9c490d23ef12e07f2dfd6d2af729cf56e6752
vansys12 <vansys12@gmail.com>
2023-04-27 16:40:11 -0400

    prueba
    prueba

---
+++ data.js
@@ -0,0 +1,311 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-puertda2d",
+      "name": "puertda2d",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -0.0084441833840323,
+        "pitch": 0.08633693566110523,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7319116345247068,
+          "pitch": 0.5497092794479244,
+          "rotation": 4.71238898038469,
+          "target": "4-vitrina-puerta"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-vitrina-2",
+      "name": "vitrina 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -3.1060153890996673,
+        "pitch": 0.06332994218049848,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9498830450179874,
+          "pitch": 0.43806523231298655,
+          "rotation": 3.9269908169872414,
+          "target": "4-vitrina-puerta"
+        },
+        {
+          "yaw": 1.6136644715665556,
+          "pitch": 0.8809979542988913,
+          "rotation": 0.7853981633974483,
+          "target": "5-vitrina-1"
+        },
+        {
+          "yaw": -0.16795293138058653,
+          "pitch": 0.7008373209715266,
+          "rotation": 0,
+          "target": "3-vitrina-5"
+        },
+        {
+          "yaw": 0.9655388689705191,
+          "pitch": 0.7899296757160883,
+          "rotation": 5.497787143782138,
+          "target": "2-vitrina-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-vitrina-4",
+      "name": "vitrina 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -0.28254646399033057,
+        "pitch": -0.16129330811166298,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5246948154343638,
+          "pitch": 0.7047793152940667,
+          "rotation": 5.497787143782138,
+          "target": "5-vitrina-1"
+        },
+        {
+          "yaw": 2.259543190856692,
+          "pitch": 0.5998410797888454,
+          "rotation": 5.497787143782138,
+          "target": "1-vitrina-2"
+        },
+        {
+          "yaw": -2.7080881677218542,
+          "pitch": 0.6116508938753427,
+          "rotation": 7.0685834705770345,
+          "target": "3-vitrina-5"
+        },
+        {
+          "yaw": 2.533633489670784,
+          "pitch": 0.21827174275178152,
+          "rotation": 3.9269908169872414,
+          "target": "0-puertda2d"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-vitrina-5",
+      "name": "vitrina 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 0.5638438111086082,
+        "pitch": 0.07266804154190609,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.130640083705476,
+          "pitch": 0.5123834231293216,
+          "rotation": 0,
+          "target": "5-vitrina-1"
+        },
+        {
+          "yaw": -2.6526970785576314,
+          "pitch": 0.5960109629404275,
+          "rotation": 0,
+          "target": "1-vitrina-2"
+        },
+        {
+          "yaw": 2.830868156597214,
+          "pitch": 0.6812822920652106,
+          "rotation": 4.71238898038469,
+          "target": "2-vitrina-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-vitrina-puerta",
+      "name": "vitrina puerta",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 2.7924323150131034,
+        "pitch": 0.11579960236580078,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7701832071330248,
+          "pitch": 0.3929684578877879,
+          "rotation": 0.7853981633974483,
+          "target": "0-puertda2d"
+        },
+        {
+          "yaw": 2.7417325897304448,
+          "pitch": 0.5029835125737705,
+          "rotation": 1.5707963267948966,
+          "target": "1-vitrina-2"
+        },
+        {
+          "yaw": 2.69409832157233,
+          "pitch": 0.3307044103555974,
+          "rotation": 0.7853981633974483,
+          "target": "2-vitrina-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-vitrina-1",
+      "name": "vitrina 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -2.1700140134026373,
+        "pitch": -0.021050168098849298,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2967718828912247,
+          "pitch": 0.8204632427683798,
+          "rotation": 10.995574287564278,
+          "target": "1-vitrina-2"
+        },
+        {
+          "yaw": 0.35109929601546597,
+          "pitch": 0.5455856213179047,
+          "rotation": 7.0685834705770345,
+          "target": "3-vitrina-5"
+        },
+        {
+          "yaw": 0.978163778367998,
+          "pitch": 0.8534641530180149,
+          "rotation": 1.5707963267948966,
+          "target": "2-vitrina-4"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "TiendaSuperMall",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
